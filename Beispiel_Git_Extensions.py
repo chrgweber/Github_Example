1ce4075287e304592e2c7258adb9860e80767443
Christoph Weber <christoph.weber@fh-kiel.de>
2024-10-22 11:34:28 +0200

    V5.0 Release

--- Beispiel_Git_Extensions.py
+++ Beispiel_Git_Extensions.py
@@ -6,8 +6,10 @@ c=a+b
 d=a*b*4
 f=c+d
 g=f+3
+h=g+3
 #Ausgabe
 print("Ergebnis von c", c)
 print("Ergebnis von d", d)
 print("Ergebnis von f", f)
 print("Ergebnis von g", g)
+print("Ergebnis von h", h)
